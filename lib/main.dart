47cf6cdfa4637849601e2410a26eecaaf2256c1b
ARKIN Eren <eren.arkin@GB42.local>
2024-03-26 17:53:03 +0100

    TP1

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  TextStyle _tS = TextStyle(
-    color: Colors.black,
-    fontSize: 12,
-  );
-
   const MyHomePage({super.key, required this.title});
 
   final String title;
@@ -38,44 +33,75 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 10;
   String _phrase = "Clic avant l'explosion";
 
-  void _decrementCounter() {
-    if (_counter > 0) {
+  TextStyle _tS = TextStyle(color: Colors.green, fontSize: 16);
+
+  Color _couleur = Color.fromARGB(255, 255, 255, 255);
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    void _decrementCounter() {
+      if (_counter > 0) {
         setState(() {
           _counter--;
         });
       } else {
         setState(() {
+          _tS = TextStyle(color: Colors.red, fontSize: 24);
+          _couleur = Color.fromARGB(255, 255, 242, 0);
           _phrase = "BOUM !";
         });
       }
-  }
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_phrase',
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Container(
+          color: _couleur,
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '$_phrase',
+                style: _tS,
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          )),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _decrementCounter,
+          tooltip: 'Decrement',
+          child: const Icon(Icons.history),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Accueil',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bed),
+              label: 'Dormir',
             ),
           ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _decrementCounter,
-        tooltip: 'Decrement',
-        child: const Icon(Icons.history),
-      ),
-    );
+      );
+    }
   }
 }
